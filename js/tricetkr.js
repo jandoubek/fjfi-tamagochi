aa6c9e979034ea10f7b63553373882cf85e35ea5
Vít Hanousek <vithanousek@seznan.cz>
2014-12-09 12:04:24 +0100

    Oprava sdálení FB aby se neodkazovalo na HTTPS

--- js/tricetkr.js
+++ js/tricetkr.js
@@ -9,7 +9,7 @@ var tricetkr = {
 				this.done=1;
 				var message='<h1><center> Dokonči jsi první semestr </center></h1> <br>';
 				message+='<h5>Tvůj jaderňák odstudoval 30 kreditů, takže postupuje do dalšího semsetru!</h5>';
-				message+='<iframe src="//www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fkmlinux.fjfi.cvut.cz%2F%7Ehanouvit%2Ffjfi-tamagochi%2FFB%2Ftricetkr.html&amp;layout=button" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
+				message+='<iframe src="//www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fkmlinux.fjfi.cvut.cz%2F%7Ehanouvit%2Fjadernak%2Ffjfi-tamagochi%2FFB%2Ftricetkr.html&amp;layout=button" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
 				jadernak.ShowAlert(message);
 				//jadernak.Timer=setTimeout("jadernak.Update()", timeout);	
 			}
