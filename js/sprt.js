92d3ebb08b647573eb17d2a4aa1eb2cc3da3345c
Vít Hanousek <vithanousek@seznan.cz>
2014-12-15 23:02:54 +0100

    Uprava adres ve větvi grafika_dev

--- js/sprt.js
+++ js/sprt.js
@@ -8,7 +8,7 @@ var sprt = {
 			{
 				this.done=1;
 				var message='<h1><center> Jsi Šprt! Dosáhl jsi 90 Znalostí </center></h1> <br>';
-				message+='<iframe src="http://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fkmlinux.fjfi.cvut.cz%2F%7Ehanouvit%2Fjadernak%2Ffjfi-tamagochi%2FFB%2Fsprt.html&amp;layout=button" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
+				message+='<iframe src="http://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fkmlinux.fjfi.cvut.cz%2F%7Ehanouvit%2Fdev%2Ffjfi-tamagochi%2FFB%2Fsprt.html&amp;layout=button" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
 				jadernak.ShowAlert(message);
 				//jadernak.Timer=setTimeout("jadernak.Update()", timeout);	
 			}
--- js/tricetkr.js
+++ js/tricetkr.js
@@ -9,7 +9,7 @@ var tricetkr = {
 				this.done=1;
 				var message='<h1><center> Dokonči jsi první semestr </center></h1> <br>';
 				message+='<h5>Tvůj jaderňák odstudoval 20 kreditů, takže postupuje do dalšího semsetru!</h5>';
-				message+='<iframe src="http://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fkmlinux.fjfi.cvut.cz%2F%7Ehanouvit%2Fjadernak%2Ffjfi-tamagochi%2FFB%2Ftricetkr.html&amp;layout=button" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
+				message+='<iframe src="http://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fkmlinux.fjfi.cvut.cz%2F%7Ehanouvit%2Fdev%2Ffjfi-tamagochi%2FFB%2Ftricetkr.html&amp;layout=button" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
 				jadernak.ShowAlert(message);
 				//jadernak.Timer=setTimeout("jadernak.Update()", timeout);	
 			}
